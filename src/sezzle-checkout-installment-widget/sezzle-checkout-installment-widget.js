3d3c6eda57b78eaaa7b1f65c2bbba424692adeba
Andy DuBois <andydubois89@gmail.com>
2022-05-03 13:20:27 -0500

    MINT-206 changed how MODAL payment dates and amounts render so there are inside the same wrapper so a screen reader will read them grouped together properly

--- src/sezzle-checkout-installment-widget/sezzle-checkout-installment-widget.js
+++ src/sezzle-checkout-installment-widget/sezzle-checkout-installment-widget.js
@@ -537,20 +537,9 @@ function renderInstallmentWidget(checkoutTotal, serviceRegion, currencySymbol){
 		installmentContainer.appendChild(installmentPlanContainer);
 
 		// creates the installment date elements
-		//function createPaymentPlan (date){
-		//	var dateElement = document.createElement('span');
-		//	dateElement.className ='sezzle-payment-date';
-		//	dateElement.innerText = date;
-        //    dateElement.tabIndex = 0;
-		//	document.querySelector('.sezzle-payment-schedule-frequency').appendChild(dateElement);
-		//}
-
-
         function createPaymentPlan (date, i){
             var installmentElement = document.querySelectorAll(".sezzle-installment-amount")
 			installmentElement[i].innerText = installmentElement[i].innerText + "\n" + date;
-
-			//document.querySelector('.sezzle-payment-schedule-frequency').appendChild(dateElement);
 		}
 
 		// parses today's date to calculate each installment date
@@ -646,16 +635,17 @@ function renderInstallmentWidget(checkoutTotal, serviceRegion, currencySymbol){
 		installmentWrapper.appendChild(sampleSchedule);
 
 		// creates each installment
-		for(var i = 0; i < 4; i++){
-			var payment = document.createElement('span');
+        for(var i = 0; i < 4; i++){
+			var payment = document.querySelector(".sezzle-modal-payment-percent").getElementsByTagName("span")
 			if (i === 0){
-				payment.innerHTML = translation[language].today;
-			} else if(interval === 30){
-				payment.innerHTML = (i*30) + " " + translation[language].days;
-			} else {
-				payment.innerHTML = translation[language].week + " " + (i*2);
+				payment[i].innerText = payment[i].innerText + "\n" + translation[language].today;
+			}  else if( i > 0 && interval === 30){
+				payment[i].innerText = payment[i].innerText + (i*30) + " " + translation[language].days;
+			} else if (i > 0){
+                payment[i].innerText = payment[i].innerText + "\n" + translation[language].week + " " + (i*2); 
+            }else {
+				payment.innerTextinnerText = translation[language].week + "\n" + " " + (i*2);
 			}
-			sampleSchedule.appendChild(payment);
 		}
 
 		// creates the info icon to open the modal
