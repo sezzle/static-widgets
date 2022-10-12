f171e600b621aa79b01f4dd0fd587118cbf52513
Andy DuBois <andydubois89@gmail.com>
2022-10-12 09:32:13 -0500

    changed to display != none, and made quotation mark use consistent.

--- src/sezzle-checkout-button/sezzle-checkout-button-asset.js
+++ src/sezzle-checkout-button/sezzle-checkout-button-asset.js
@@ -139,7 +139,7 @@ class SezzleCheckoutButton {
 		const checkoutButtons = document.getElementsByClassName('additional-checkout-buttons').length ? document.getElementsByClassName('additional-checkout-buttons') : document.getElementsByName('checkout');
 		for (let i = 0; i < checkoutButtons.length; i++) {
             var buttonStyle = getComputedStyle(checkoutButtons[i]);
-			if (checkoutButtons[i].className === "additional-checkout-buttons" && buttonStyle.display === 'block' && buttonStyle.visibility === 'visible') {
+			if (checkoutButtons[i].className === 'additional-checkout-buttons' && buttonStyle.display != 'none' && buttonStyle.visibility === 'visible') {
 				checkoutButtons[i].appendChild(sezzleCheckoutButton)
 			} else {
 				const checkoutButtonParent = checkoutButtons[i].parentElement ? checkoutButtons[i].parentElement : checkoutButtons[i];
