cda81bad1ac8151e41d0823b541de4d2b22a0c94
Allyson Hammerlund <allyson.hammerlund@sezzle.com>
2024-03-28 15:12:16 -0500

    create button scripts

---
+++ internals/scripts/pull_button_translations.mjs
@@ -0,0 +1,127 @@
+import { LokaliseApi } from "@lokalise/node-api";
+import chalk from "chalk";
+import fs from "fs";
+import https from "https";
+import AdmZip from "adm-zip";
+import { Headers } from "node-fetch";
+
+global.Headers = Headers;
+const basePath = "src/sezzle-checkout-button/translations";
+const tmpFileName = "translations.zip";
+
+const pathToDownloadFile = `${basePath}/${tmpFileName}`;
+
+const apiKey = process.env.API_KEY;
+if (!apiKey) {
+    throw new Error(chalk.red("Localise API Key is missing"));
+}
+
+const projectId = process.env.PROJECT_ID;
+if (!projectId) {
+    throw new Error(chalk.red("Localise project id is missing"));
+}
+
+const lokaliseApi = new LokaliseApi({ apiKey });
+
+console.log("Start download translation files");
+lokaliseApi
+    .files()
+    .download(projectId, {
+        format: "json",
+        bundle_structure: "%LANG_ISO%.json",
+        placeholder_format: "icu",
+        original_filenames: false,
+        directory_prefix: "src/sezzle-checkout-button/translations/",
+        add_newline_eof: true,
+        json_unescaped_slashes: true,
+        indentation: "4sp",
+    })
+    .then((response) =>
+        // download zip file by URL which was returned from localise
+        downloadFile(response.bundle_url, pathToDownloadFile)
+    )
+    .then((resultFilePath) => {
+        const resultFile = new AdmZip(resultFilePath);
+
+        try {
+            // extract files to the directory
+            resultFile.extractAllTo(basePath, true);
+            console.log(
+                "🎉 🎉 🎉 🎉  Translation files were downloaded successfully"
+            );
+        } catch (err) {
+            console.error(err);
+        } finally {
+            // remove zip file
+            fs.unlink(resultFilePath, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    })
+    .then((_) => {
+        formatFrenchTranslationFiles();
+        formatSpanishTranslationFiles();
+    })
+    .catch((reason) => {
+        console.error(reason);
+    });
+
+function downloadFile(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest);
+        https
+            .get(url, (response) => {
+                response.pipe(file);
+                file.on("finish", () => {
+                    file.close();
+                    resolve(dest);
+                });
+            })
+            .on("error", (err) => {
+                fs.unlink(dest);
+                reject(err.message);
+            });
+    });
+}
+
+const replaceFRSpecificCharacters = (messages) =>
+    // this regex replace the normal space around french punctuation marks like
+    // ?!:;«» with the non-breaking space to make sure that the punctuation mark
+    // and the preceding word won't be seperated by line change during typesetting
+    Object.entries(messages).reduce(
+        (acc, [key, val]) => ({
+            ...acc,
+            [key]: val
+                .replace(/\s(?=[?!:;»])/g, "\u00a0")
+                .replace(/«\s/g, "«\u00a0"),
+        }),
+        {}
+    );
+
+function formatSpanishTranslationFiles() {
+    const filePath = `${basePath}/es.json`;
+    const esTransParsed = JSON.parse(fs.readFileSync(filePath).toString());
+    saveToFile(esTransParsed, filePath);
+}
+
+function formatFrenchTranslationFiles() {
+    const filePath = `${basePath}/fr.json`;
+    const frTransParsed = JSON.parse(fs.readFileSync(filePath).toString());
+    const frTransFormatted = replaceFRSpecificCharacters(frTransParsed);
+    saveToFile(frTransFormatted, filePath);
+}
+
+function saveToFile(TransFormatted, filePath) {
+    try {
+        fs.writeFileSync(
+            filePath,
+            JSON.stringify(TransFormatted, null, 2),
+            "utf8"
+        );
+        console.log("Formatted translations successfully saved to disk");
+    } catch (error) {
+        console.log("An error has occurred ", error);
+    }
+}
---
+++ internals/scripts/push_button_translations.mjs
@@ -0,0 +1,41 @@
+import { LokaliseApi } from "@lokalise/node-api";
+import fs from "fs";
+import { Headers } from "node-fetch";
+
+global.Headers = Headers;
+
+const basePath = "src/sezzle-checkout-button/translations";
+const locale = process.env.LOCALE || "en";
+
+const translationFile = `${basePath}/${locale}.json`;
+console.log("Used translation file to upload: ", translationFile);
+
+const apiKey = process.env.API_KEY;
+if (!apiKey) {
+    console.log("Localise API Key is missing");
+    process.exit(1);
+}
+
+const projectId = process.env.PROJECT_ID;
+if (!projectId) {
+    console.log("Localise project id is missing");
+    process.exit(1);
+}
+
+const lokaliseApi = new LokaliseApi({ apiKey });
+const file = fs.readFileSync(translationFile);
+
+lokaliseApi
+    .files()
+    .upload(projectId, {
+        data: file.toString("base64"),
+        filename: `${locale}.json`,
+        lang_iso: locale,
+    })
+    .then((result) => {
+        console.log("🎉 🎉 🎉 🎉  translations pushed to lokalise!!!");
+        console.log(result);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
